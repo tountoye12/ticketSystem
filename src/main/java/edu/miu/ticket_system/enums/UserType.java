714582822f8ad7cad70dc4f0cf82d4db2c3682ed
Dheynoshan Nadarajah <dheynoshan@gmail.com>
2024-12-18 15:24:23 -0600

    Removed lombok and Administrator and LeaseOfficeStaff entities

--- src/main/java/edu/miu/ticket_system/enums/UserType.java
+++ src/main/java/edu/miu/ticket_system/enums/UserType.java
@@ -1,5 +1,21 @@
 package edu.miu.ticket_system.enums;
 
+import edu.miu.ticket_system.entity.Ticket;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
 public enum UserType {
-    LEASE_OFFICE_STAFF, RESIDENT, MAINTENANCE_STAFF, ADMINISTRATOR
+    LEASE_OFFICE_STAFF, RESIDENT, MaintenanceStaff, ADMINISTRATOR;
+
+
+    private static class MaintenanceStaff{
+        private Specialization specialization;
+        private List<Ticket> assignedTicket;
+
+    }
+
+
 }
+
+
