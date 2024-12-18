7b6d74100399d2267401085f3a487666b017a85b
Diallo <mamadouadama609@gmail.com>
2024-12-18 15:59:29 -0600

    Docker files

--- db/database.sh
+++ db/database.sh
@@ -1,3 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
-docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=ticketdatabase  -p 3306:3306 -d mysql:latest
+docker run \
+  --name mysql-container \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -e MYSQL_DATABASE=ticketdatabase \
+  -p 3306:3306 \
+  -d \
+  mysql:latest
