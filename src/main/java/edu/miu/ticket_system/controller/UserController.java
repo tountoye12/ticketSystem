b51210330251cd27b3a9fa25da780b4bdde14eb8
Dheynoshan Nadarajah <dheynoshan@gmail.com>
2024-12-18 11:25:44 -0600

    fix merge conflicts

--- src/main/java/edu/miu/ticket_system/controller/UserController.java
+++ src/main/java/edu/miu/ticket_system/controller/UserController.java
@@ -1,4 +1,18 @@
 package edu.miu.ticket_system.controller;
 
+import edu.miu.ticket_system.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return null;
+    }
 }
