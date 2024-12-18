7f976d5e76b5e35365e24ff2de0cd5c738d9b183
Dheynoshan Nadarajah <dheynoshan@gmail.com>
2024-12-18 16:22:40 -0600

    Lombok pom.xml update

--- src/main/java/edu/miu/ticket_system/enums/UserType.java
+++ src/main/java/edu/miu/ticket_system/enums/UserType.java
@@ -1,21 +1,8 @@
 package edu.miu.ticket_system.enums;
 
-import edu.miu.ticket_system.entity.Ticket;
-import jakarta.persistence.OneToMany;
-
-import java.util.List;
 
 public enum UserType {
-    LEASE_OFFICE_STAFF, RESIDENT, MaintenanceStaff, ADMINISTRATOR;
-
-
-    private static class MaintenanceStaff{
-        private Specialization specialization;
-        private List<Ticket> assignedTicket;
-
-    }
-
-
+    LEASE_OFFICE_STAFF, RESIDENT, MAINTENANCE_STAFF, ADMINISTRATOR;
 }
 
 
