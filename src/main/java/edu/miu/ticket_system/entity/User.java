e86547a45afb7c097919ea7fbea99c225a86e953
Diallo <mamadouadama609@gmail.com>
2024-12-18 21:02:31 -0600

    Done for User fields validations

--- src/main/java/edu/miu/ticket_system/entity/User.java
+++ src/main/java/edu/miu/ticket_system/entity/User.java
@@ -2,10 +2,11 @@ package edu.miu.ticket_system.entity;
 
 import edu.miu.ticket_system.enums.UserType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
@@ -16,16 +17,40 @@ import java.time.LocalDateTime;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "First name cannot be empty")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String userName;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
+    @NotEmpty(message = "Contact number cannot be empty")
+    @Pattern(regexp = "^[0-9]{10,15}$", message = "Contact number must be between 10 and 15 digits")
     private String contactNumber;
+
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "User type cannot be null")
     private UserType userType;
+
+    @PastOrPresent(message = "Created date cannot be in the future")
     private LocalDateTime createdAt;
+
+    @PastOrPresent(message = "Updated date cannot be in the future")
     private LocalDateTime updatedAt;
 }
